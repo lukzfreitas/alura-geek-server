2fba055ed6bf2cb497c85711db26cbc79c316fe2
Lucas Freitas <lucas.antunes@fiergs.org.br>
2022-10-11 21:30:19 -0300

    test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import process from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  const app: INestApplication = await NestFactory.create(AppModule);
+  app.enableCors();
+  await app.listen(process.env.Port || 3000);
 }
 bootstrap();
