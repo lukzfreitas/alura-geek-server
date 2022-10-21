21a63841f82a0cbf0d5dc0f0b401a2bc7dea5fd1
Lucas Freitas <lucas.antunes@fiergs.org.br>
2022-10-21 18:01:42 -0300

    passing paramenters methods to enable CORS

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app: INestApplication = await NestFactory.create(AppModule, {cors: true});
+  const app: INestApplication = await NestFactory.create(AppModule);
+  app.enableCors({
+    methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
+  })
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
