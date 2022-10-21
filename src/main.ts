7c8c7c266826a9e2cff253958b7f52c6d42a785b
Lucas Freitas <lucas.antunes@fiergs.org.br>
2022-10-21 17:53:04 -0300

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app: INestApplication = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app: INestApplication = await NestFactory.create(AppModule, {cors: true});
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
