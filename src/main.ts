c394bf039e28bfd7e757cf344f8230e194fe6ed6
Lucas Freitas <lucas.antunes@fiergs.org.br>
2022-10-23 17:09:54 -0300

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app: INestApplication = await NestFactory.create(AppModule);
-  app.enableCors({
-    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-    methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
-  })
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
