9911f0de0f84dbcc2a98c17043c581f99782fa2a
Lucas Freitas <lucas.antunes@fiergs.org.br>
2022-12-28 15:45:40 -0300

    adjusts CORS

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { QueryParams } from '../models/query-params.model';
 import { Category } from './category.schema';
 import { CategoryService } from './category.service';
+import { Public } from 'src/auth/auth.constant';
 
 @Controller()
 export class CategoryController {
@@ -12,22 +13,26 @@ export class CategoryController {
     return this.categoryService.create(category);
   }
 
+  @Public()
   @Get('/category')
   async findAll(): Promise<Category[]> {
     return this.categoryService.findAll();
   }
 
+  @Public()
   @Get('/category/products')
   async findByCategory(@Query() query: any) {
     query = new QueryParams(query);
     return this.categoryService.findCategoriesAndProducts(query);
   }
 
+  @Public()
   @Get('/category/:code')
   async findByCode(@Param('code') code: string): Promise<Category> {
     return this.categoryService.findByCode(Number.parseInt(code));
   }
 
+  @Public()
   @Get('/category/:code/products')
   async findByCodeProducts(@Param('code') code: string) {
     return this.categoryService.findByCodeProducts(Number.parseInt(code));
