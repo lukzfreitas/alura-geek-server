d8026f1ae8e3038b90cd6776ac79456d2c397096
Lucas Freitas <lucas.antunes@fiergs.org.br>
2022-12-28 15:25:39 -0300

    adjusts CORS

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -4,37 +4,41 @@ import {
   Post,
   HttpStatus,
   HttpCode,
-  // UseGuards,
+  UseGuards,
 } from '@nestjs/common';
-// import { Public } from '../auth/auth.constant';
-import { AuthService } from '../auth/auth.service';
+import { Public } from '../auth/auth.constant';
+// import { AuthService } from '../auth/auth.service';
 import { GetCurrentUserUsername } from '../auth/get-current-user-username.decorator';
 import { GetCurrentUser } from '../auth/get-current-user.decorator';
-// import { RefreshTokenAuthGuard } from '../auth/guards/refresh-token-auth.guard';
+import { RefreshTokenAuthGuard } from '../auth/guards/refresh-token-auth.guard';
 
 @Controller('auth')
 export class LoginController {
-  constructor(private authService: AuthService) {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
 
   // @Public()
   @Post('login')
   async login(@Body() { username, password }) {
-    return this.authService.login(username, password);
+    return { username, password };
+    // return this.authService.login(username, password);
   }
 
   @Post('logout')
   async logout(@Body() { username }) {
-    this.authService.logout(username);
+    return username;
+    // this.authService.logout(username);
   }
 
-  // @Public()
-  // @UseGuards(RefreshTokenAuthGuard)
+  @Public()
+  @UseGuards(RefreshTokenAuthGuard)
   @Post('refresh')
   @HttpCode(HttpStatus.OK)
   async refresh(
     @GetCurrentUserUsername() username: string,
     @GetCurrentUser('refreshToken') refreshToken: string,
   ) {
-    return this.authService.refreshTokens(username, refreshToken);
+    return { username, refreshToken };
+    // return this.authService.refreshTokens(username, refreshToken);
   }
 }
--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-// import { AuthModule } from '../auth/auth.module';
+import { AuthModule } from '../auth/auth.module';
 import { LoginController } from './login.controller';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [LoginController],
 })
 export class LoginModule {}
