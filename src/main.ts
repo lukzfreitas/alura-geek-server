c0e0f12f0b2471397b3543d694b523ef49cc5475
Lucas Freitas <lucas.antunes@fiergs.org.br>
2022-12-28 09:35:19 -0300

    adjusts enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app: INestApplication = await NestFactory.create(AppModule);
-  app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: ['https://alura-geek-omega.vercel.app/', 'http://localhost:3000/'],
-    credentials: true,
-  });
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
